cd222f37cc3080d68fc50539bc58dfe2981c2822
Teddy <mickey23405383@gmail.com>
2018-12-05 16:25:19 +0800

    ad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,6 +24,7 @@ import { ForgottenComponent} from './forgotten/forgotten.component';
 import { ResetComponent } from './reset/reset.component';
 import { WebmailComponent } from './webmail/webmail.component';
 import { ShoppingOrderComponent } from './shopping-order/shopping-order.component';
+import { ADComponent } from './ad/ad.component';
 const routes: Routes = [
   {
     path : '', component : LayoutComponent,
@@ -140,6 +141,10 @@ const routes: Routes = [
   },
   {
     path : 'register', component : RegisterComponent
+  },
+  {
+    path : 'ad',
+    component : ADComponent,
   },
   {
     path : 'profile/:userId',
