af26f5f8c408b20eaf18d4d5fea8989add552acb
Axot <asdswegreh@gmail.com>
2018-12-07 17:42:31 +0800

    更改image map和header路徑

--- src/app/image-map/image-map.component.ts
+++ src/app/image-map/image-map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-image-map',
@@ -7,11 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageMapComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, public dataService: DataService) { }
 
   ngOnInit() {
   }
-  ck() {
-    window.location.reload();
+  ck(n) {
+    window.document.body.scrollTop = 0;
+    window.document.documentElement.scrollTop = 0;
+    this.router.navigate(['/catalog/catalogGrid/', n, 0]);
+    this.dataService.ChangeCategory(n);
+  }
+  search(searchElement) {
+    this.router.navigate(['/catalog/catalogGrid/', searchElement, 0, 'search']);
+    this.dataService.search(searchElement);
   }
 }
